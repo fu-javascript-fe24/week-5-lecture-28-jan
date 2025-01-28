d059199a20f2a7796475200d25fd5d908a2607d9
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-01-28 10:25:47 +0100

    create renderUsers funciton

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ console.log('Hello World!');
 
 function renderUsers() {
     const bodyRef = document.querySelector('body');
+
+    for(let user of globalUsers) {
+        const pRef = document.createElement('p');
+        pRef.textContent = user.username;
+        bodyRef.appendChild(pRef);
+        console.log(pRef);
+    }
 }
